962be707c31a5054ead2e736dc3907b679085f2e
synapticsurge <synapticsurge@proton.me>
2025-05-14 16:45:15 +0530

    fix: removed unused code

--- refactor/models/marks_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:flutter/foundation.dart';
-part 'marks_model.freezed.dart';
-
-@freezed
-sealed class MarksModel with _$MarksModel {
-  factory MarksModel({required List<Map<String, String>> marks}) = _MarksModel;
-}
